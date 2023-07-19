50818c182606f23ee1ce41ee6cf0fb352cae6307
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-07-20 02:37:58 +0900

    ✨ Feat: npm install http express cors

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+var app = express();
+var server = require('http').createServer(app);
+var bodyParser = require('body-parser');
 
-/* GET home page. */
-router.get('/test', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send({ success: true} );
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api', (req, res) => {
+  const code = req.body.code;
+  console.log(code);
+  res.send("확인!!")
+})
+
+server.listen(8080, () => {
+  console.log('server is running on 8080')
 });
 
-module.exports = router;
+/* GET home page. */
+// router.get('/test', function(req, res, next) {
+//   // res.render('index', { title: 'Express' });
+//   res.send({ success: true} );
+// });
+
+// module.exports = router;
