680071d2936414e8abe3ac43f65d945658453a34
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-07-24 20:25:10 +0900

    ✨ 카카오 로그인 인가코드 수령 + jwt 디코딩 -> 회원정보 추출 가능

--- routes/index.js
+++ routes/index.js
@@ -13,32 +13,42 @@ router.post('/test', (req, res) => {
   const loginCode = req.body.code;
   console.log(loginCode);
 
-  const details = {
-    grant_type: 'authorization_code',
-    client_id: 'e6c2fe139670b147caaf750b558a4750', // REST API KEY
-    redirect_uri: 'http://localhost:3000/kakao-login',
-    code: loginCode,
-  };
-
-  const formBody = Object.keys(details)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(details[key]))
-    .join('&');
+  
+  const grantType = 'authorization_code'
+  const clientId = 'e6c2fe139670b147caaf750b558a4750' // REST API KEY
+  const redirectUri = 'http://localhost:3000/kakao-login'
+  const code = loginCode
+  
 
   axios({
     method: 'post',
-    url: 'https://kauth.kakao.com/oauth/token/',
+    url: `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${clientId}&redirect_uri=${redirectUri}&code=${code}`,
     headers: {
       'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
     },
-    data: formBody,
   })
     .then((response) => {
-      const jwtToken = response.data.jwt;
-      console.log('JWT발급 성공', jwtToken);
-      res.json({ jwtToken });
+      console.log(JSON.stringify(response.data));
+      const {access_token} = response.data
+      axios.post(
+          `https://kapi.kakao.com/v2/user/me`,
+          {},
+          {
+              headers:{
+                  Authorization: `Bearer ${access_token}`,
+                  "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+              }
+          }
+      ).then((response) => {
+          console.log('진짜진짜몬가몬가', response.data);
+          res.send("로그인 성공!!")
+      }).catch((Error)=>{
+          console.log(Error);
+      })
     })
     .catch((error) => {
       console.log('JWT토큰 발급에 실패했습니다.', error);
+      res.send("로그인 실패..")
       // res.status(500).json({ error: 'JWT토큰 발급에 실패했습니다.' });
     });
 });
