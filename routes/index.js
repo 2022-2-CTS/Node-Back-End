5a43ab7a6e7609990162e013f2d42c798da845f7
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-07-28 20:14:18 +0900

    ✨ Feat: 네이버 자동 로그인 + 유저정보 디비에 저장

--- routes/index.js
+++ routes/index.js
@@ -134,61 +134,54 @@ router.post('/kakaoLogin', (req, res) => {
       const {access_token} = response.data;
       kakaoAccessToken = access_token;
       axios.post(
-          `https://kapi.kakao.com/v2/user/me`,
-          {},
-          {
-              headers:{
-                  Authorization: `Bearer ${access_token}`,
-                  "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-              }
-          }
-      ).then((response) => {
-          console.log('진짜진짜몬가몬가', response.data.id);
-          console.log('진짜진짜몬가몬가', response.data.kakao_account.profile.nickname);
-          
-          const kakaoId = (response.data.id).toString()
-          const kakaoNickname = response.data.kakao_account.profile.nickname;
-
-          var sqlFind = 'SELECT ID FROM kakaouser WHERE ID = ?;';
-          var sqlInsert = 'INSERT INTO kakaouser (ID, NICKNAME) VALUES (?, ?);';
-          maria.query(sqlFind, kakaoId, function(err, rows, fields){
-            if(!err){
-              console.log("여기야?")
-              console.log(rows.length)
-              console.log(fields)
-              if (rows.length == 0){
-                // res.send("success")
-                console.log("여기도?")
-                maria.query(sqlInsert, [kakaoId, kakaoNickname], function(err, rows, feilds){
-                  if(!err){
-                    console.log('디비에 저장 성공')
-                    console.log(rows)
-                  }else{
-                    console.log("Error : ", err)
-                  }
-                })
-              }else{
-                res.send("로그인 성공!!")
-              }
-            }else{
-              console.log("error", err)
+        `https://kapi.kakao.com/v2/user/me`,
+        {},
+        {
+            headers:{
+                Authorization: `Bearer ${access_token}`,
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
             }
-          })
-        }).catch((err) => {
-          console.log(err)
+        }
+      ).then((response) => {
+        console.log('진짜진짜몬가몬가', response.data.id);
+        console.log('진짜진짜몬가몬가', response.data.kakao_account.profile.nickname);
+        
+        const kakaoId = (response.data.id).toString()
+        const kakaoNickname = response.data.kakao_account.profile.nickname;
+
+        var sqlFind = 'SELECT ID FROM kakaouser WHERE ID = ?;';
+        var sqlInsert = 'INSERT INTO kakaouser (ID, NICKNAME) VALUES (?, ?);';
+        maria.query(sqlFind, kakaoId, function(err, rows, fields){
+          if(!err){
+            console.log("여기야?")
+            console.log(rows.length)
+            console.log(fields)
+            if (rows.length == 0){
+              // res.send("success")
+              console.log("여기도?")
+              maria.query(sqlInsert, [kakaoId, kakaoNickname], function(err, rows, feilds){
+                if(!err){
+                  console.log('디비에 저장 성공')
+                  console.log(rows)
+                }else{
+                  console.log("Error : ", err)
+                }
+              })
+            }else{
+              res.send("로그인 성공!!")
+            }
+          }else{
+            console.log("error", err)
+          }
         })
-          // res.send("로그인 성공!!")
-      }).catch((Error)=>{
-          console.log(Error);
+      }).catch((err) => {
+        console.log(err)
       })
+        // res.send("로그인 성공!!")
+    }).catch((Error)=>{
+        console.log(Error);
     })
-//     .catch((error) => {
-//       console.log('JWT토큰 발급에 실패했습니다.', error);
-//       res.send("로그인 실패..")
-//       // res.status(500).json({ error: 'JWT토큰 발급에 실패했습니다.' });
-//     });
-//   });
-// });
+  })
 
 router.get('/kakaoLoginToken', (req, res) => {
   res.send(kakaoAccessToken)
@@ -196,8 +189,10 @@ router.get('/kakaoLoginToken', (req, res) => {
   
 
 
-
 //네이버 로그인 구현부
+
+var naverAccessToken = ''
+
 router.post('/naverLogin', (req, res) =>  {
   const loginCode = req.body.code;
   const stateCode = req.body.state;
@@ -220,6 +215,8 @@ router.post('/naverLogin', (req, res) =>  {
     console.log(JSON.stringify(response.data))
     const {token_type} = response.data
     const {access_token} = response.data
+    naverAccessToken = access_token;
+    console.log(access_token)
     axios.post(
       'https://openapi.naver.com/v1/nid/me',
       {},
@@ -230,7 +227,34 @@ router.post('/naverLogin', (req, res) =>  {
       }
     ).then((response) => {
       console.log('이번에도 진짜진짜 몬가몬가다', response.data);
-      res.send("로그인 성공!!")
+
+      const naverId = response.data.response.id
+      const naverNickname = response.data.response.nickname
+
+      console.log(naverId, naverNickname)
+
+      var sqlFind = 'SELECT ID FROM naveruser WHERE ID = ?;';
+      var sqlInsert = 'INSERT INTO naveruser (ID, NICKNAME) VALUES (?, ?);';
+
+      maria.query(sqlFind, naverId, function(err, rows, feilds){
+        if(!err){
+          if(rows.length == 0){
+            maria.query(sqlInsert, [naverId, naverNickname], function(err, rows, feilds){
+              if(!err){
+                console.log('디비에 저장 성공')
+                console.log(rows)
+              }else{
+                console.log("insert error ", err)
+              }
+            })
+          }else{
+            res.send("로그인 성공!!")
+          }
+        }else{
+          console.log("find error", err)
+        }
+      })
+      // res.send("로그인 성공!!")
     }).catch((Error) => {
       res.send("진짜루..?")
       console.log(Error);
@@ -244,6 +268,35 @@ router.post('/naverLogin', (req, res) =>  {
   });
 });
 
+router.get('/naverLoginToken', (req, res) => {
+  res.send(naverAccessToken)
+})
+
+router.post('/naverLoginTokenAccess', (req, res) => {
+  const naverToken = req.body.naverAccessToken
+  console.log(naverToken)
+  axios.get(
+    "https://openapi.naver.com/v1/nid/me",
+    {
+      headers:{
+        Authorization : `Bearer ${naverToken}`,
+      },
+    }
+  )
+  .then((response) => {
+    console.log(response.data); // 서버 응답의 데이터를 출력합니다.
+    if(response.data.code != -401){
+      console.log("나한테")
+      res.send('valid')
+    }else{
+      console.log("왜이래")
+      res.send('invalid')
+    }
+  })
+  .catch((err) => {
+    console.log("err", err); // 오류가 발생한 경우 오류를 출력합니다.
+  });
+})
 
 //구글 로그인 구현부
 router.post('/googleLogin', (req, res) => {
