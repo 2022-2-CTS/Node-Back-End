b0b613f6f65db564beb0dc99a40a57b573bdc4b8
leejyohan <86225337+leejyohan@users.noreply.github.com>
2023-08-01 22:15:12 +0900

    🔨 Refactor: DB저장 코드 수정

--- routes/apidata/api.js
+++ routes/apidata/api.js
@@ -8,81 +8,29 @@ app.use(bodyParser.json())
 
 const conn = require('../../database/connect/maria');
 
+const val =['concert','musical','play','exhibit']
+
 var sql = 'INSERT INTO apiData (category, url, title,st_dt,ed_dt,showtime,price,poster,location) values (?,?,?,?,?,?,?,?,?)';
 
-var tmpData = require('../event/json/play_json.json');
-for (let i = 0; i < tmpData.length; i++) {
-  var Par = [
-    tmpData[i].category, 
-    tmpData[i].url, 
-    tmpData[i].data.title, 
-    tmpData[i].data.op_st_dt, 
-    tmpData[i].data.op_ed_dt, 
-    tmpData[i].data.showtime, 
-    tmpData[i].data.price, 
-    tmpData[i].data.imgSrc, 
-    tmpData[i].data.location]
-  conn.query(sql, Par, function (err, rows, fields) {
-    if (err) {
-      console.log(err)
-    }
-  })
+for(let j=0;j<val.length;j++){
+  var tmpData = require('../event/json/'+val[j]+'_json.json');
+  for (let i = 0; i < tmpData.length; i++) {
+    var Par = [
+      tmpData[i].category, 
+      tmpData[i].url, 
+      tmpData[i].data.title, 
+      tmpData[i].data.op_st_dt, 
+      tmpData[i].data.op_ed_dt, 
+      tmpData[i].data.showtime, 
+      tmpData[i].data.price, 
+      tmpData[i].data.imgSrc, 
+      tmpData[i].data.location]
+    conn.query(sql, Par, function (err, rows, fields) {
+      if (err) {
+        console.log(err)
+      }
+    })
+  }
 }
 
-tmpData= require('../event/json/concert_json.json');
-for (let i = 0; i < tmpData.length; i++) {
-  var Par = [
-    tmpData[i].category, 
-    tmpData[i].url, 
-    tmpData[i].data.title, 
-    tmpData[i].data.op_st_dt, 
-    tmpData[i].data.op_ed_dt, 
-    tmpData[i].data.showtime, 
-    tmpData[i].data.price, 
-    tmpData[i].data.imgSrc, 
-    tmpData[i].data.location]
-  conn.query(sql, Par, function (err, rows, fields) {
-    if (err) {
-      console.log(err)
-    }
-  })
-}
-
-tmpData= require('../event/json/exhibit_json.json');
-for (let i = 0; i < tmpData.length; i++) {
-  var Par = [
-    tmpData[i].category, 
-    tmpData[i].url, 
-    tmpData[i].data.title, 
-    tmpData[i].data.op_st_dt, 
-    tmpData[i].data.op_ed_dt, 
-    tmpData[i].data.showtime, 
-    tmpData[i].data.price, 
-    tmpData[i].data.imgSrc, 
-    tmpData[i].data.location]
-  conn.query(sql, Par, function (err, rows, fields) {
-    if (err) {
-      console.log(err)
-    }
-  })
-}
-
-tmpData= require('../event/json/musical_json.json');
-for (let i = 0; i < tmpData.length; i++) {
-  var Par = [
-    tmpData[i].category, 
-    tmpData[i].url, 
-    tmpData[i].data.title, 
-    tmpData[i].data.op_st_dt, 
-    tmpData[i].data.op_ed_dt, 
-    tmpData[i].data.showtime, 
-    tmpData[i].data.price, 
-    tmpData[i].data.imgSrc, 
-    tmpData[i].data.location]
-  conn.query(sql, Par, function (err, rows, fields) {
-    if (err) {
-      console.log(err)
-    }
-  })
-}
 module.exports = router;
