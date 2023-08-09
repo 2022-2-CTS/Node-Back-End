76c291ec8c304b2d9b0876be179584495c44bff7
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-08-09 19:41:12 +0900

    ✨ Feat: 찜 목록 api 생성

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,39 @@ const maria = require('../database/connect/maria');
 const crypto = require('crypto-js');
 
 
+router.post("/favoriteUpload", (req, res) => {
+    const id = req.body.id;
+    const url = req.body.url;
 
+    var sql = 'INSERT INTO fList (ID, url) VALUES (?,?)';
+
+    maria.query(sql, [id, url], function(err, rows, fields){
+        if(!err){
+            console.log("DB저장 성공")
+        }else{
+            console.log("DB저장 실패")
+        }
+    })
+})
+
+router.get("/favoritDownload", (req, res) => {
+    const id = req.body.id;
+    
+    var sql = "SELECT url FROM fList WHERE ID = ?;"
+
+    maria.query(sql, id, function(err, rows, feilds){
+        if(!err){
+            if(rows.length == 0){
+                res.send("0");
+            }else{
+                res.send(rows);
+            }
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
 
 
 module.exports = router;
