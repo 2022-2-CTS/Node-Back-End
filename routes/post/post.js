059d9bb424048200fb763e76c58cf84ed7563500
leejyohan <chyun1529@gmail.com>
2023-08-14 10:40:31 +0900

    ✨ Feat: 작성된글 get api 작성

--- routes/post/post.js
+++ routes/post/post.js
@@ -10,9 +10,10 @@ const axios = require('axios');
 
 const conn = require('../../database/connect/maria');
 
-var sql = 'INSERT INTO POST (ID, TITLE, DATE, LOCATION, TAG, CONTENT) values ( ?, ?, ?, ?, ?, ? )';
+//작성한 글 DB 저장
+router.post('/write', (req, res) => {
 
-router.post('/posts', (req, res) => {
+    var sql = 'INSERT INTO POST (ID, TITLE, DATE, LOCATION, TAG, CONTENT) values ( ?, ?, ?, ?, ?, ? )';
 
     var data = [
         req.userId,
@@ -29,6 +30,39 @@ router.post('/posts', (req, res) => {
         }
         else{
             console.log("저장")
+        }
+    })
+
+})
+
+//DB에 작성된 글 가져오기
+router.get('/lists', (req, res) => {
+
+    var sql = 'SELECT * FROM POST';
+
+    conn.query(sql, data, function (err, rows) {
+        if (err) {
+            console.log(err)
+        }
+        else{
+            let writeData = [];
+
+            for( let i = 0; i < rows.length; i++ )
+            {
+                let Data = {
+                    idx:i, //글 인덱스 생성
+                    title: rows[i].title, // 제목
+                    location: rows[i].location, // 행사 장소(주소)
+                    date: rows[i].date, // 작성 일자
+                    tag: rows[i].tag, // 태그
+                    content:rows[i].content //글내용
+
+                }
+                writeData.push(Data);
+            }
+            writeData = JSON.stringify(writeData)
+
+            res.send(writeData)
         }
     })
 
