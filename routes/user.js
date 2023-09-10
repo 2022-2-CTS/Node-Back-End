7c93431f6be8ac47ccdb2a316820968a10d1a51b
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-09-10 19:09:12 +0900

    🚑 Fix:찜 목록 저장 기능 수정

--- routes/user.js
+++ routes/user.js
@@ -17,46 +17,68 @@ const crypto = require('crypto-js');
 
 //찜 목록 저장하기
 router.post("/favorites", (req, res) => {
-    const json = req.body.favoriteObject;
-    
+    try{
+        const json = req.body.favoriteObject;
 
-    const objData = [
-        json.userId,
-        json.event.title,
-        json.event.location,
-        json.event.startDate,
-        json.event.endDate,
-        json.event.time,
-        json.event.price,
-        json.event.src
-    ]
+        var objData = []
 
-    for (var i=0; i<objData.length; i++){
-        if(objData[i] == null || objData[i] == ""){
-            res.status(400).json({
-                status: "fail",
-                data: {
-                    msg: "행사 찜하기에 실패했습니다. 다시 시도해주세요."
-                }
-            })
-            return
+        if (typeof(json) == "string"){
+            const obj = JSON.parse(json)
+            console.log(typeof(obj))
+            objData = [
+                obj.userId,
+                obj.event.title,
+                obj.event.location,
+                obj.event.startDate,
+                obj.event.endDate,
+                obj.event.time,
+                obj.event.price,
+                obj.event.src
+            ]
+        }else{
+            objData = [
+                json.userId,
+                json.event.title,
+                json.event.location,
+                json.event.startDate,
+                json.event.endDate,
+                json.event.time,
+                json.event.price,
+                json.event.src
+            ]
         }
-    }
     
-    var sql = 'INSERT INTO FAVORITE (ID, TITLE, LOCATION, STARTDATE, ENDDATE, TIME, PRICE, SRC) VALUES (?,?,?,?,?,?,?,?)';
-
-    maria.query(sql, objData, function(err, rows, fields){
-        try{
-            if(!err){
-                res.status(200).json({status:"success", data:{result:"true"}})
-            }else{
-                res.status(200).json({status:"success", data:{result:"false", data:"행사 찜하기에 실패했습니다. 다시 시도해주세요."}})
-                console.log(err)
+    
+        for (var i=0; i<objData.length; i++){
+            if(objData[i] == null || objData[i] == ""){
+                res.status(400).json({
+                    status: "fail",
+                    data: {
+                        msg: "행사 찜하기에 실패했습니다. 다시 시도해주세요."
+                    }
+                })
+                return
             }
-        }catch(err){
-            res.status(500).json({status:"error", msg:"서버 오류 발생"})
         }
-    })
+        
+        var sql = 'INSERT INTO FAVORITE (ID, TITLE, LOCATION, STARTDATE, ENDDATE, TIME, PRICE, SRC) VALUES (?,?,?,?,?,?,?,?)';
+    
+        maria.query(sql, objData, function(err, rows, fields){
+            try{
+                if(!err){
+                    res.status(200).json({status:"success", data:{result:"true"}})
+                }else{
+                    res.status(200).json({status:"success", data:{result:"false", data:"행사 찜하기에 실패했습니다. 다시 시도해주세요."}})
+                    console.log(err)
+                }
+            }catch(err){
+                res.status(500).json({status:"error", msg:"서버 오류 발생"})
+            }
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
 })
 
 
