227970a75dbec66de50af15b3c768f853a255725
leejyohan <86225337+leejyohan@users.noreply.github.com>
2023-09-10 18:26:24 +0900

    🚑 Fix:글작성 400에러 수정

--- routes/post.js
+++ routes/post.js
@@ -11,7 +11,7 @@ const axios = require('axios');
 const conn = require('../database/connect/maria');
 
 //작성한 글 DB 저장
-router.post('/write', (req) => {
+router.post('/write', (req, res) => {
 
     const data = [
         req.body.Writedata.userId,
@@ -24,32 +24,39 @@ router.post('/write', (req) => {
 
     const sql = 'INSERT INTO POST (ID, TITLE, DATE, LOCATION, TAG, CONTENT) values ( ?, ?, ?, ?, ?, ? )';
 
-    conn.query(sql, data, function ( req, res ) {
-        try {
-            if (data === undefined) {
-
-                res.status(400).json({
-                    status: "fail",
-                    data: {
-                        msg: "글 작성에 실패했습니다. 다시 시도해주세요."
-                    }
-                });
+    conn.query(sql, data, function (err, rows, fields) {
+        if (req.body.Writedata.userId == null ||
+            req.body.Writedata.title == null ||
+            req.body.Writedata.date == null ||
+            req.body.Writedata.location == null ||
+            req.body.Writedata.tag == null ||
+            req.body.Writedata.content == null) {
                 
-                return;
+            console.log("400")
+            res.status(400).json({
+                status: "fail",
+                data: {
+                    msg: "글 작성에 실패했습니다. 다시 시도해주세요."
+                }
+            });
+
+            return;
+        }
+        else {
+            try {
+                console.log("200")
+                res.status(200).json({
+                    status: "success",
+                    data: null
+                })
+            } catch (err) {
+                console.log("500")
+                res.status(500).json({
+                    status: "error",
+                    msg: "서버 오류 발생"
+                })
+
             }
-
-            res.status(200).json({
-                status: "success",
-                data: null
-            })
-
-        } catch ( error ) {
-
-            res.status(500).json({
-                status: "error",
-                msg: "서버 오류 발생"
-            })
-
         }
     })
 
@@ -64,27 +71,26 @@ router.get('/lists', (req, res) => {
         if (err) {
             console.log(err)
         }
-        else{
+        else {
 
             let writeData = [];
 
-            for( let i = 0; i < rows.length; i++ )
-            {
+            for (let i = 0; i < rows.length; i++) {
                 let Data = {
-                    idx:i, //글 인덱스 생성
+                    idx: i, //글 인덱스 생성
                     title: rows[i].TITLE, // 제목
                     location: rows[i].LOCATION, // 행사 장소(주소)
                     date: rows[i].DATE, // 작성 일자
                     tag: rows[i].TAG, // 태그
-                    content:rows[i].CONTENT //글내용
+                    content: rows[i].CONTENT //글내용
 
                 }
-                
+
                 writeData.push(Data);
             }
 
             writeData = JSON.stringify(writeData)
-            
+
             try {
 
                 res.status(200).json({
