3f4b479ed470ca80ef7b55fe2c8c80740bed0b35
Miseon <soup7912@gmail.com>
2023-10-21 20:56:08 +0900

    👽️ Update: cors 수정

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ var checkRouter = require('./routes/check')
 var app = express();
 // ADD: cors
 var cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',      // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
+}));
 
 var maria = require('./database/connect/maria');
 maria.connect();
